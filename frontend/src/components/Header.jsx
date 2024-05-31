e604f578b125784e6d420fee828c35860bea1d38
hydrazin60 <sureshkhdk45@gmail.com>
2024-05-31 22:51:23 +0545

    normal change

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,19 +1,31 @@
 import React from "react";
-import { useNavigate } from 'react-router-dom';
+import { useNavigate } from "react-router-dom";
 
 export default function Header() {
-    const navigate = useNavigate();
-    
-    return (
-        <div className="bg-zinc-300">
-            <div className="flex justify-between items-center mx-auto p-3">
-                <h1 className="font-bold cursor-pointer" onClick={() => navigate("/")}>MINI_PROJECT</h1>
-                <ul className="flex gap-6">
-                    <li className="cursor-pointer" onClick={() => navigate("/")}>Home</li>
-                    <li className="cursor-pointer" onClick={() => navigate("/about")}>About</li>
-                    <li className="cursor-pointer" onClick={() => navigate("/sign-in")}>Sign-In</li>
-                </ul>
-            </div>
-        </div>
-    );
+  const navigate = useNavigate();
+
+  return (
+    <div className="bg-zinc-900 text-white">
+      <div className="flex justify-between items-center mx-auto p-3">
+        <img
+          src="logo.png"
+          className="h-14 rounded-full  cursor-pointer "
+          alt="logo"
+          onClick={() => navigate("/")}
+        />
+        <h1 className="font-bold">MINI_PROJECT</h1>
+        <ul className="flex gap-6">
+          <li className="cursor-pointer" onClick={() => navigate("/")}>
+            Home
+          </li>
+          <li className="cursor-pointer" onClick={() => navigate("/about")}>
+            About
+          </li>
+          <li className="cursor-pointer" onClick={() => navigate("/sign-in")}>
+            Sign-In
+          </li>
+        </ul>
+      </div>
+    </div>
+  );
 }
