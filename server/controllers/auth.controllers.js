731d4e32a6e029cd4ede9bb83fb7b3ec7bb9c281
hydrazin60 <sureshkhdk45@gmail.com>
2024-06-04 11:01:08 +0545

    add small  for expires token in '24h'

--- server/controllers/auth.controllers.js
+++ server/controllers/auth.controllers.js
@@ -1,5 +1,5 @@
 import User from "../models/user.model.js";
-import jwt from "jsonwebtoken"
+import jwt from "jsonwebtoken";
 import bcryptjs from "bcryptjs";
 import { errorHandler } from "../utils/error.js";
 export const signup = async (req, res, next) => {
@@ -32,24 +32,27 @@ export const signup = async (req, res, next) => {
 };
 export default signup;
 
-export const signin =  async(req ,res , next) =>{
-  const {email , password} = req.body;
-  if(!email || !password){
-    return next(errorHandler(400 , "All Filds Are required"))
+export const signin = async (req, res, next) => {
+  const { email, password } = req.body;
+  if (!email || !password) {
+    return next(errorHandler(400, "All Filds Are required"));
   }
-  try{
-const valitUser = await  User.findOne({email})
-if(!valitUser) return next(errorHandler(404 , 'User Not Found !'));
-  const  validPassword = bcryptjs.compareSync(password , valitUser.password)
-if(!validPassword) return next(errorHandler(404 , "Invalide Password"));
-const token = jwt.sign({id : valitUser._id}, process.env.JWT_SECRET)
-// const token = jwt.sign({ id: foundUser._id }, process.env.JWT_SECRET, {
-//   expiresIn: '1h', // Add an expiration time for better security
-// });
-const {password : hashPassword , ...userResponse} = valitUser._doc
-res.cookie('access_token' , token , {httpOnly: true} ).status(200).json({ message : "Login sucessful" , user : userResponse})
+  try {
+    const valitUser = await User.findOne({ email });
+    if (!valitUser) return next(errorHandler(404, "User Not Found !"));
+    const validPassword = bcryptjs.compareSync(password, valitUser.password);
+    if (!validPassword) return next(errorHandler(404, "Invalide Password"));
+    // const token = jwt.sign({ id: valitUser._id }, process.env.JWT_SECRET);
+    const token = jwt.sign({ id: foundUser._id }, process.env.JWT_SECRET, {
+      expiresIn: '24h', // Add an expiration time for better security
+    });
+    const { password: hashPassword, ...userResponse } = valitUser._doc;
+    res
+      .cookie("access_token", token, { httpOnly: true })
+      .status(200)
+      .json({ message: "Login sucessful", user: userResponse });
   } catch (err) {
     console.error(err);
-    next(errorHandler(500, 'Internal Server Error'));
+    next(errorHandler(500, "Internal Server Error"));
   }
 };
